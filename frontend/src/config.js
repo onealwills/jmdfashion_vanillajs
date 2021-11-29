091fb0d50bb8e37612ac6b72794b38d8b3667321
williams <oneal.wills@gmail.com>
2021-11-29 14:29:10 +0100

    update package

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
-export const apiUrl = 'http://localhost:5000';
+export const apiUrl = document.location.href.startsWith('http://localhost')
+  ? 'http://localhost:5000'
+  : '';
